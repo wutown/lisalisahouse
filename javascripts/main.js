d18d0702ee64cb72095f56b1ebb493a5b6df9dfc
wutown <wutown@gmail.com>
2014-06-17 14:11:52 +0800

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,12 @@
 console.log('This would be the main JS file.');
+
+
+function DontClickMyFace() {
+    var image = document.getElementById('faceid');
+    if (image.src.match("face")) {
+        image.src = "images/house.jpg";
+    } else {
+        image.src = "images/face.jpg";
+    }
+}
+
